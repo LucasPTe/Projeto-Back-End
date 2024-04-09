e3d4bee82abc8e6b1dd32a65a258414799d4121a
Hugo Daniel <hugoestudos2000@gmail.com>
2024-04-08 21:55:28 -0300

    Tela de cadastro e login:
    Fiz novamente ás duas telas, no entanto, ajeitei o HTML e retirei códigos desnecessários do HTML e CSS, dessa forma, ficará mais fácil de vincular o banco de dados.

--- Cadastro/cadastro.php
+++ Cadastro/cadastro.php
@@ -5,10 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tela de Cadastro</title>
+    <title>Cadastro Med+</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="/Tela inicial/archives/telecall.png">
-    <link rel="shortcut icon" href="/Tela inicial/archives/telecall.png" type="image/x-icon">
     <link rel="stylesheet" href="cadastro.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -27,31 +25,33 @@
         <div class="row border rounded-5 p-3 bg-white shadow box-area" id="box-area">
             
             <div class="input-group mb-3">
-                    <a href="/Tela inicial/index.html"><input type="button" value="Tenho Login" class="btn btn-primary botao_card"></a>
+                <a class="btn btn-primary botao_card" href="http://localhost/Projeto-Back-End/Login/Login.php" role="button">Tenho Login</a>
             </div>
             
-            <p style="font-size: 25px;" class="d-flex justify-content-center cadastro_titulo" id="cadastro_titulo">CADASTRE-SE</p>
+            <p class="d-flex justify-content-center cadastro_titulo" id="cadastro_titulo">CADASTRE-SE</p>
 
             <!--Left Box-->
 
             <div class="col-md-6 right-box" id="right-box">
                 <div class="row align-items-center">
-                    <label for="nome_completo" id="labelNome">Nome completo</label>
+                    
+                    <label for="nome_completo" id="labelNome">Nome completo</label>                   
                     <div class="input-group mb-3">
                         <input type="text" class="form-control form-control-lg bg-light fs-6" id="nome_completo" minlength="15" maxlength="80" required placeholder="">
                     </div>
                     
-                    <label for="e-mail" id="labelEmail">E-mail</label>
-                    <div class="input-group mb-3">
-                        <input type="email" class="form-control form-control-lg bg-light fs-6" required id="E-mail" placeholder="">
-                    </div>
-                    
-                    <label for="data_de_nascimento" id="label-data">Data de Nascimento</label>
+                    <label for="data_de_nascimento" id="label-data">Data de Nascimento</label>                  
                     <div class="input-group mb-3">
                         <input type="date" class="form-control form-control-lg bg-light fs-6" id="data" required placeholder="">
                     </div>
+
+                    <label for="e-mail" id="e-mail">E-mail</label>
+                    <div class="input-group mb-3">
+                        <input type="e-mail" class="form-control form-control-lg bg-light fs-6" required id="e-mail" placeholder="">
+                    </div>
                     
                     <div class="input-group mx-1">
+                        
                         <form class="pb-3 form_sexo" id="sexo">
                             <p class="sexo_titulo" id="sexo_titulo">Sexo</p>
                             <input required type="radio" name="Sexo" value="Masculino" id="masculino">
@@ -61,11 +61,12 @@
                             <input required type="radio" name="Sexo" value="Prefiro_nao_informar" id="nao_informar">
                             <label for="prefiro_não_informar">Prefiro não informar</label>
                         </form>
+                    
                     </div>
                     
-                    <label for="nome_da_mae" id="labelMae">Nome Materno</label>
+                    <label for="nome_da_mae" id="labelMae">Nome da Mãe</label>
                     <div class="input-group mb-3">
-                        <input type="text" class="form-control form-control-lg bg-light fs-6" id="mae" required placeholder="">
+                        <input type="text" class="form-control form-control-lg bg-light fs-6" id="mae" minlength="15" maxlength="80" required placeholder="">
                     </div>
                     
                     <label for="CPF" id="labelCpf">CPF</label>
@@ -73,15 +74,16 @@
                         <input type="text" class="form-control form-control-lg bg-light fs-6" required id="cpf" placeholder="">
                     </div>
                     
-                    <label for="numero_de_celular" id="labelCelular">Telefone Celular</label>
+                    <label for="numero_de_celular" id="labelCelular">Número de Celular</label>
                     <div class="input-group mb-3">
-                        <input type="tel" class="form-control form-control-lg bg-light fs-6" required id="celular" placeholder="(+55) XX XXXXX-XXXX">
+                        <input type="tel" class="form-control form-control-lg bg-light fs-6" required id="celular" placeholder="">
                     </div>
-                </div>
 
-                <div class="botao">
-                    <button type="button" class="btn btn-primary btn-lg">Cadastrar Dados</button>
-                    <button type="button" class="btn btn-secondary btn-lg">Limpar Dados</button>    
+                    <label for="telefone_fixo" id="labelFixo">Telefone Fixo</label>
+                    <div class="input-group mb-3">
+                        <input type="tel" class="form-control form-control-lg bg-light fs-6" required id="fixo" placeholder="">
+                    </div>
+                
                 </div>
 
             </div>
@@ -89,64 +91,54 @@
             <!--Right Box-->
             <div class="col-md-6 right-box" id="right-box">
                 <div class="row align-items-center">
-                    <label for="telefone_fixo" id="labelFixo">Telefone Fixo</label>
+
+                    <label for="CEP" id="labelCep">CEP</label>
                     <div class="input-group mb-3">
-                        <input type="tel" class="form-control form-control-lg bg-light fs-6" required id="fixo" placeholder="(+55) XX XXXX-XXXX">
+                        <input type="text" class="form-control form-control-lg bg-light fs-6" required id="CEP" placeholder="">
                     </div>
-                    <label for="endereco" id="labelRua">Endereço</label>
+
+                    <label for="bairro" id="labelBairro">Bairro</label>
                     <div class="input-group mb-3">
-                        <input type="text" class="form-control form-control-lg bg-light fs-6" required id="endereco" placeholder="">
+                        <input type="text" class="form-control form-control-lg bg-light fs-6" required id="bairro" placeholder="">
                     </div>
-                    <label for="endereco" id="labelBairro">Bairro</label>
+
+                    <label for="estado" id="labelEstado">Estado</label>
                     <div class="input-group mb-3">
-                        <input type="text" class="form-control form-control-lg bg-light fs-6" required id="endereco" placeholder="">
+                        <input type="text" class="form-control form-control-lg bg-light fs-6" required id="estado" placeholder="">
                     </div>
-                    <label for="endereco" id="labelNumero">Número</label>
+
+                    <label for="endereco" id="labelRua">Endereço e N°</label>
                     <div class="input-group mb-3">
-                        <input type="text" class="form-control form-control-lg bg-light fs-6" required id="endereco" placeholder="">
+                        <input type="text" class="form-control form-control-lg bg-light fs-6" required id="endereco" placeholder="Ex: Rua Castro, n° 105">
                     </div>
-                    <label for="endereco" id="labelCEP">CEP</label>
-                    <div class="input-group mb-3">
-                        <input type="text" class="form-control form-control-lg bg-light fs-6" required id="endereco" placeholder="">
-                    </div>
+
                     <label for="nome_de_usuario" id="labelUsuario">Login</label>
                     <div class="input-group mb-3">
                         <input type="text" class="form-control form-control-lg bg-light fs-6" minlength="6" maxlength="6" required id="usuario" placeholder="">
                     </div>
+
                     <label for="senha" id="labelSenha">Senha</label>
                     <div class="input-group mb-3">
                         <input type="password" class="form-control form-control-lg bg-light fs-6" required id="senha" minlength="8" maxlength="8" placeholder="">
                     </div>
+
                     <label for="confirmar_senha" id="labelConfirmSenha">Confirmar Senha</label>
                     <div class="input-group mb-3">
                         <input type="password" class="form-control form-control-lg bg-light fs-6" required id="confirmSenha" minlength="8" maxlength="8" placeholder="">
                     </div>
-                    <div class="input-group mb-5 d-flex justify-content-between">
-                    </div>
-                </div>
 
-            <!-- Modal -->
-            <div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Mensagem</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" onclick="redirect()"></button>
-                        </div>
-                        <div class="modal-body">
-                            <p id="modalMessage"></p>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" onclick="redirect()">Fechar</button>
-                        </div>
-                    </div>
                 </div>
+                
+                <div style="margin-top: 57px;" class="d-grid gap-2 d-md-block">
+                <button style="font-size: 18px;" class="btn btn-primary" type="button">Cadastre-se</button>
+                <button style="font-size: 18px;" class="btn btn-primary" type="button">Limpar Dados</button>
+                </div>
+            
             </div>
 
         </div>
         <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.js" integrity="sha512-0XDfGxFliYJPFrideYOoxdgNIvrwGTLnmK20xZbCAvPfLGQMzHUsaqZK8ZoH+luXGRxTrS46+Aq400nCnAT0/w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-        <script src="/Login/cadastro.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
