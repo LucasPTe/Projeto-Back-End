efac46c97b7182a05c7945fc7008ab51e95a7331
Hugo Daniel <hugoestudos2000@gmail.com>
2024-05-08 16:15:37 -0300

    Concertei um caminho:
    Ajeitei o caminho da tela de login pra tela do cliente, quando o usuário for acessar sua conta.

--- Tela Inicial/tela_inicial.php
+++ Tela Inicial/tela_inicial.php
@@ -8,8 +8,7 @@
     <title>Dr.Agenda</title>
     <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-    <link rel="shortcut icon" href="/Tela inicial/archives/telecall.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link rel="stylesheet" href="style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -59,7 +58,7 @@
                     <button class="btn btn-primary border-3 m-0 mx-1 btn-fonte botao_a" id="decreaseFontBtn">A - </button>
                 </div>
                 <button class="btn btn-danger ms-2 sair_botao" onclick="sair()">Sair</button>
-                <a href="/Projeto-Back-End/Login/Login.php" class="login-button">Área do Cliente</a>
+                <a href="/Projeto-Back-End/Login/Login.php" class="login-button">Login</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
