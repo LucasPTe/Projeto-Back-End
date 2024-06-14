124144e23509d13f6d3a3195ca5b9c8428abcfe6
Lucas Lopes <128646570+LucasPTe@users.noreply.github.com>
2024-06-14 11:02:37 -0300

    Nova tela do Master

--- master.php
+++ master.php
@@ -3,10 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Usuário Master</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <a href="landingPage/index.php">Inicio</a>
-    <a href="consulta/config.php">Tela de LOG</a>
+    <div class="master-container">
+        <h1 class="welcome-message">Bem-vindo, Usuário Master</h1>
+        <p class="description">Aqui você pode acessar todas as configurações e funcionalidades do sistema.</p>
+        <div class="button-group">
+            <a class="master-btn" href="http://localhost/Projeto-Back-End/landingPage/index.php">Início</a>
+            <a class="master-btn" href="http://localhost/Projeto-Back-End/consulta/config.php">Tela de LOG</a>
+        </div>
+    </div>
 </body>
 </html>
